a296124873d4081e36da2bad5d78f488e084acec
arifrizqy <m.arifrizqy.f@gmail.com>
2023-11-24 13:11:40 +0700

    initialize git & github

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PinjamPengembalianController;
+use App\Http\Controllers\UserAdminController;
+use App\Http\Controllers\VisitorController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::controller(UserAdminController::class)->group(function () {
+    Route::get('/admin-list', 'index');
+});
+
+Route::controller(VisitorController::class)->group(function () {
+    Route::get('/visitors', 'index');
+});
+
+Route::controller(ItemController::class)->group(function () {
+    Route::get('/items', 'index');
+});
+
+Route::controller(PinjamPengembalianController::class)->group(function () {
+    Route::get('/pinjam-pengembalian', 'index');
+});
